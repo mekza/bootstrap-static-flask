c62bc81436e097d753fa449ae5014a40db222b07
Martin-Zack Mekkaoui <martin@mekkaoui.fr>
2013-10-13 15:05:13 +0200

    Fix static_from_root and add 404 template

--- site.py
+++ site.py
@@ -6,10 +6,14 @@ site.py
 
 It's crappy but it works.
 """
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+    
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
 @app.route('/favicon.ico')
